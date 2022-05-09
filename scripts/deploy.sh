cd8f2d11a2ef123a5b158fb2b32912c8744b504d
km7865 <rjsxo789@naver.com>
2022-05-09 18:04:05 +0900

    BUILD: Automate deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> Check current operating application."
 
-CURRENT_PID=$(pgrep -fl dongwon-springboot2-webservice | grep jar | awk '{pr    int $1}')
+CURRENT_PID=$(pgrep -fl dongwon-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "Current operating application's PID: $CURRENT_PID"
 
@@ -33,6 +33,6 @@ chmod +x $JAR_NAME
 echo "> Execute $JAR_NAME."
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/ap    plication-real.properties,/home/ec2-user/app/application-oauth.properties,/h    ome/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
