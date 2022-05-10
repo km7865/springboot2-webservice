aa456d95585d56c0aba9d7fd7231b7d9ea19e6d8
km7865 <rjsxo789@naver.com>
2022-05-10 17:18:18 +0900

    BUILD: Add deployment scripts

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전활할 Port: $IDLE_PORT"
     echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-rul.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
     echo "> 엔진엑스 Reload"
     sudo service nginx reload
 }
