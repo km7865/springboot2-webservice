85a0429a7e60a8e51692cc78bd8740d4fa3da7ea
km7865 <rjsxo789@naver.com>
2022-05-10 14:47:53 +0900

    BUILD: Update SecurityConfig

--- src/main/java/com/dongwon/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/dongwon/book/springboot/config/auth/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final CustomOauth2UserService customOauth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,15 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile")
-                .permitAll().antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
-                .logout().logoutSuccessUrl("/")
-                    .and()
-                        .oauth2Login()
-                            .userInfoEndpoint()
-                                .userService(customOauth2UserService);
-
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
